04b717ca2818e9c232c9e88966b7caec63ca5e33
w3cj <cj@null.computer>
2019-12-07 20:40:01 -0700

    :sparkles::bug: Add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);

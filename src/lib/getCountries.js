0109b8e5076ce3f500d0e43723960941e43ae2a8
w3cj <cj@null.computer>
2022-09-27 13:07:17 -0600

    :sparkles: use flags metadata min

--- src/lib/getCountries.js
+++ src/lib/getCountries.js
@@ -5,7 +5,7 @@ let countries;
 async function getCountries() {
   if (countries) return countries;
   countries = (async () => {
-    const { data } = await axios.get('https://iso-3166-flags.netlify.app/dist/metadata.json');
+    const { data } = await axios.get('https://iso-3166-flags.netlify.app/dist/metadata.min.json');
     const result = new Map();
     data.forEach((info) => {
       const item = {
